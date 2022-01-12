d21fe8ce4c8802bdd0e17be0701efca15f6978ef
jsomiya <jainsomiya0@gmail.com>
2022-01-12 14:49:43 +0530

    response data added

--- app.py
+++ app.py
@@ -16,16 +16,23 @@ db.init_app(app)
 
 @app.route('/postqueryresponse', methods=['POST'])
 def getquery():
+    url = "citymall.dev"
     request_data = request.get_json()
     user_phone = request_data['user_phone']
+    no_of_users = len(user_phone)
     template_id = request_data['template_id']
     query_data = Querylist.query.get_or_404(template_id)
     sql = query_data.query_text
-    result = db.session.execute(sql, {'val':user_phone})
-    res = result.one_or_none()
-    data = list(res)
-    url = "citymall.dev"
-    response  = requests.post('http:/'+url+'//api/ct/webhook',data)
+    for i in range(0,no_of_users):
+        result = db.session.execute(sql, {'val':user_phone[i]})
+        res = result.one_or_none()
+        parameters = list(res)
+        data = {
+            "numbers": user_phone,
+            "template_id": template_id,
+            "parameters":parameters
+        }
+        response  = requests.post('http://'+url+'//api/ct/webhook',data)
     return response.status_code
 
 
