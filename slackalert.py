7ec5af289e881f3bf836e9ec5a6ab8250ba2c4fb
jsomiya <jainsomiya0@gmail.com>
2022-01-17 20:22:26 +0530

    errors addded

--- slackalert.py
+++ slackalert.py
@@ -1,3 +1,5 @@
+import requests
+
 def sendSlackMessage(message):
     payload = '{"text":"%s"}' % message
     response = requests.post('https://hooks.slack.com/services/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', data=payload)
