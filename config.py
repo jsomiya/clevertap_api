bbb03b504582cc97ba59f72e3f0a35989b4c789e
jsomiya <jainsomiya0@gmail.com>
2022-01-24 11:53:23 +0530

    passed environ var

--- config.py
+++ config.py
@@ -3,4 +3,10 @@ import os
 url = "http://citymall.live"
 
 slack_token = os.environ.get('SLACK_TOKEN')
+x_ct_key = os.environ.get('HEADER_TOKEN')
+
+headers = {
+        'x-ct-key': x_ct_key,
+        'Content-Type': 'application/json',
+        }
 
